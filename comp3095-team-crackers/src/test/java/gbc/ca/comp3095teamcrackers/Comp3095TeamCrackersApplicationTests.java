c27ef1641c151e4efaa1c3c8415b609d4a5e8c62
ozXoz <honourkorkmaz@gmail.com>
2023-01-19 23:03:16 -0500

    spring boot

---
+++ comp3095-team-crackers/src/test/java/gbc/ca/comp3095teamcrackers/Comp3095TeamCrackersApplicationTests.java
@@ -0,0 +1,13 @@
+package gbc.ca.comp3095teamcrackers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp3095TeamCrackersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
