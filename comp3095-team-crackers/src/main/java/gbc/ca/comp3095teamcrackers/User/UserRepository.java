32c44a22bbbcddd30b4c06e44c805b7eace88163
ozXoz <honourkorkmaz@gmail.com>
2023-01-19 23:08:14 -0500

    first assignemnt

---
+++ comp3095-team-crackers/src/main/java/gbc/ca/comp3095teamcrackers/User/UserRepository.java
@@ -0,0 +1,17 @@
+package gbc.ca.comp3095teamcrackers.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/*
+Comp3095-Team-Crackers-Assignment-1
+Team Members:
+    Safa Aru :     101331910
+    Hakan Inel :   101213098
+    Onur Korkmaz : 101303363
+ */
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
